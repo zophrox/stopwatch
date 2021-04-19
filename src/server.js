81a1293f967365c36b479b83613185b2cce02b8f
zophrox <zophrox@gmail.com>
2021-04-19 19:10:42 +0300

    port8080

--- src/server.js
+++ src/server.js
@@ -5,4 +5,4 @@ app.use(express.static(__dirname + '/dist/stopwatch'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/stopwatch/index.html'));});
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 8080);
